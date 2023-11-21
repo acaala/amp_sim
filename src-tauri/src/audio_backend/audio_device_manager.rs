21f10b544da2d8e457171a9b7281e23bbc21d3bd
Liam Watts <elnewatts@gmail.com>
2023-11-21 16:37:58 +0000

    Device manager now is only responsible for devices

--- src-tauri/src/audio_backend/audio_device_manager.rs
+++ src-tauri/src/audio_backend/audio_device_manager.rs
@@ -1,105 +1,72 @@
 use anyhow::Error;
 use cpal::{
     traits::{DeviceTrait, HostTrait},
-    Device, DevicesError,
+    Device,
 };
 
 pub struct AudioDeviceManager {
-    input_device: Device,
-    output_device: Device,
+    pub input_device: Device,
+    pub output_device: Device,
 }
 
 impl AudioDeviceManager {
     pub fn new() -> Self {
         let host = cpal::default_host();
 
-        // If has config available use that else:
         let input_device = host
             .default_input_device()
-            .expect("to have default input device");
+            .expect("To get default input device");
         let output_device = host
             .default_output_device()
-            .expect("to have default output device");
+            .expect("To get default output device");
+
         AudioDeviceManager {
             input_device,
             output_device,
         }
     }
 
-    pub fn get_input_devices() -> Option<Vec<String>> {
+    pub fn get_input_devices() -> Result<Vec<String>, Error> {
         let host = cpal::default_host();
 
-        let devices = match host.input_devices() {
-            Ok(devices) => devices.collect::<Vec<_>>(), // Store devices in a Vec
-            Err(e) => {
-                println!("Error getting input devices: {:?}", e);
-                return None;
-            }
-        };
+        let devices = host.input_devices()?;
+        let device_names: Vec<String> = devices.filter_map(|dev| dev.name().ok()).collect();
 
-        let device_names: Vec<String> = devices.iter().filter_map(|dev| dev.name().ok()).collect();
-        Some(device_names)
+        Ok(device_names)
     }
 
-    pub fn get_output_devices() -> Option<Vec<String>> {
+    pub fn get_output_devices() -> Result<Vec<String>, Error> {
         let host = cpal::default_host();
 
-        let devices = match host.output_devices() {
-            Ok(devices) => devices.collect::<Vec<_>>(), // Store devices in a Vec
-            Err(e) => {
-                println!("Error getting output devices: {:?}", e);
-                return None;
-            }
-        };
+        let devices = host.output_devices()?;
+        let device_names: Vec<String> = devices.filter_map(|dev| dev.name().ok()).collect();
 
-        let device_names: Vec<String> = devices.iter().filter_map(|dev| dev.name().ok()).collect();
-        Some(device_names)
+        Ok(device_names)
     }
 
-    pub fn set_input_device(&mut self, new_device: String) -> Result<(), String> {
+    pub fn set_input_device(&mut self, new_device_name: String) -> Result<(), Error> {
         let host = cpal::default_host();
 
-        let devices = match host.input_devices() {
-            Ok(devices) => devices.collect::<Vec<_>>(), // Store devices in a Vec
-            Err(e) => {
-                println!("Error getting input devices: {:?}", e);
-                return Err(e.to_string());
-            }
-        };
-
+        let devices = host.input_devices()?;
         let input_device = devices
             .into_iter()
-            .find(|x| x.name().unwrap() == new_device);
+            .find(|x| x.name().unwrap() == new_device_name);
 
-        match input_device {
-            Some(device) => {
-                self.input_device = device;
-                Ok(())
-            }
-            None => Err("Failed to get specified device".to_string()),
-        }
+        self.input_device = input_device.unwrap();
+
+        Ok(())
     }
-    pub fn set_output_device(&mut self, new_device: String) -> Result<(), String> {
+
+    pub fn set_output_device(&mut self, new_device_name: String) -> Result<(), Error> {
         let host = cpal::default_host();
 
-        let devices = match host.output_devices() {
-            Ok(devices) => devices.collect::<Vec<_>>(), // Store devices in a Vec
-            Err(e) => {
-                println!("Error getting input devices: {:?}", e);
-                return Err(e.to_string());
-            }
-        };
-
+        let devices = host.output_devices()?;
         let output_device = devices
             .into_iter()
-            .find(|x| x.name().unwrap() == new_device);
+            .find(|x| x.name().unwrap() == new_device_name);
 
-        match output_device {
-            Some(device) => {
-                self.output_device = device;
-                Ok(())
-            }
-            None => Err("Failed to get specified device".to_string()),
-        }
+        self.input_device = output_device.unwrap();
+
+        Ok(())
     }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,12 +3,11 @@
 
 use std::{collections::HashMap, sync::Mutex};
 
-use amp_sim::audio_backend::{
-    audio_device_manager::AudioDeviceManager, audio_processor::AudioProcessor,
-};
+use amp_sim::audio_backend::audio_device_manager::AudioDeviceManager;
+use cpal::traits::DeviceTrait;
 
 #[tauri::command]
-fn get_audio_devices() -> Result<HashMap<String, Vec<String>>, String> {
+fn get_devices() -> Result<HashMap<String, Vec<String>>, String> {
     let input_devices = AudioDeviceManager::get_input_devices().expect("To get input devices");
     let output_devices = AudioDeviceManager::get_output_devices().expect("To get output devices");
     Ok(HashMap::from([
@@ -19,10 +18,12 @@ fn get_audio_devices() -> Result<HashMap<String, Vec<String>>, String> {
 
 #[tauri::command]
 fn set_input_device(
-    state: tauri::State<Mutex<AudioDeviceManager>>,
+    state: tauri::State<'_, Mutex<AudioDeviceManager>>,
     new_device: String,
 ) -> Result<(), String> {
-    state.lock().unwrap().set_input_device(new_device)
+    state.lock().unwrap().set_input_device(new_device);
+    println!("{:#?}", state.lock().unwrap().input_device.name());
+    Ok(())
 }
 
 #[tauri::command]
@@ -30,14 +31,16 @@ fn set_output_device(
     state: tauri::State<Mutex<AudioDeviceManager>>,
     new_device: String,
 ) -> Result<(), String> {
-    state.lock().unwrap().set_output_device(new_device)
+    state.lock().unwrap().set_output_device(new_device);
+    println!("{:#?}", state.lock().unwrap().output_device.name());
+    Ok(())
 }
 
-#[tauri::command]
-async fn start_audio() {
-    let audio_processor = AudioProcessor::new();
-    audio_processor.unwrap().run(10);
-}
+// #[tauri::command]
+// fn start_audio(state: tauri::State<'_, Mutex<AudioDeviceManager>>) -> Result<(), String> {
+//     state.lock().unwrap().start_stream();
+//     Ok(())
+// }
 
 fn main() {
     let audio_device_manager = AudioDeviceManager::new();
@@ -45,10 +48,9 @@ fn main() {
     tauri::Builder::default()
         .manage(Mutex::new(audio_device_manager))
         .invoke_handler(tauri::generate_handler![
-            get_audio_devices,
-            start_audio,
             set_input_device,
-            set_output_device
+            set_output_device,
+            get_devices
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
