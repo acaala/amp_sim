edc63f235cb630a664e3170372754b98578469b2
Liam Watts <elnewatts@gmail.com>
2023-11-23 12:59:11 +0000

    Added basic styling for device

---
+++ src/lib/Devices.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { onMount } from "svelte";
+
+  let devices: {
+    inputs: Array<string>;
+    outputs: Array<string>;
+  } = {
+    inputs: [],
+    outputs: [],
+  };
+
+  let initialDevices: InitialDevices = {
+    input: "",
+    output: "",
+  };
+
+  async function startAudio() {
+    initialDevices = (await invoke("start_audio")) as InitialDevices;
+    console.log(initialDevices);
+  }
+
+  async function getDevices() {
+    devices = await invoke("get_devices");
+  }
+
+  async function setInputDevice(e: Event) {
+    const target = e.target as HTMLSelectElement;
+
+    if (target) {
+      await invoke("set_input_device", { newDevice: target.value });
+    }
+  }
+
+  async function setOutDevice(e: Event) {
+    const target = e.target as HTMLSelectElement;
+
+    if (target) {
+      await invoke("set_output_device", { newDevice: target.value });
+    }
+  }
+
+  onMount(async () => {
+    await getDevices();
+    await startAudio();
+  });
+</script>
+
+<div class="flex gap-4">
+  <div>
+    <p class="mb-1 text-sm font-medium text-gray-300">Input Device</p>
+    <select class="bg-gray-700" on:change={(e) => setInputDevice(e)}>
+      {#each devices.inputs as input}
+        <option value={input} selected={input == initialDevices.input}
+          >{input}</option
+        >
+      {/each}
+    </select>
+  </div>
+
+  <div>
+    <p class="mb-1 text-sm font-medium text-gray-300">Output Device</p>
+    <select class="bg-gray-700" on:change={(e) => setOutDevice(e)}>
+      {#each devices.outputs as output}
+        <option value={output} selected={output == initialDevices.output}
+          >{output}</option
+        >
+      {/each}
+    </select>
+  </div>
+</div>
--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,96 +1,42 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri"
+  import { invoke } from "@tauri-apps/api/tauri";
   import { onMount } from "svelte";
 
-  type InitialDevices = {
-    input: string,
-    output: string
-  }
+  let availableProcessors: Array<string> = [];
+  // let activeProcessors: Array<Object> = [];
 
-  let devices: {
-    inputs: Array<string>
-    outputs: Array<string>
-  } = {
-    inputs: [],
-    outputs: [],
-  };
-
-  let initialDevices: InitialDevices = {
-    input: "",
-    output: ""
-  }
-
-  let processors: Array<string> = [];
-
-
-  async function getDevices(){
-    devices = await invoke("get_devices");
-  }
-
-  async function getProcessors(){
-    processors = await invoke("get_processors") as [];
+  async function getAvailableProcessors() {
+    availableProcessors = (await invoke("get_processors")) as [];
   }
 
   onMount(async () => {
-    await getDevices();
-     await startAudio();
-     getProcessors();
-  })
-
-  async function startAudio() {
-    initialDevices = await invoke("start_audio") as InitialDevices;
-    console.log(initialDevices);
-  }
-
-  async function setInputDevice(e: Event) {
-    const target = e.target as HTMLSelectElement;
-
-    if (target) {
-      await invoke("set_input_device", { newDevice: target.value });
-    }
-  }
-  async function setOutDevice(e: Event) {
-    const target = e.target as HTMLSelectElement;
-
-    if (target) {
-      await invoke("set_output_device", { newDevice: target.value });
-    }
-  }
-
+    getAvailableProcessors();
+  });
 
   async function addProcessor(e: Event) {
     const target = e.target as HTMLButtonElement;
 
     if (target) {
-      let processorDetails = await invoke("add_processor_to_pipeline", { name: target.innerText.toLowerCase() })
+      let processorDetails = await invoke("add_processor_to_pipeline", {
+        name: target.innerText.toLowerCase(),
+      });
       console.log(processorDetails);
     }
   }
 
   async function updateProcessorValues() {
-   let newValues = await invoke("update_processor_values", { processorName: "amplifier", values: { "volume": 1.0 } })
-   console.log(newValues);
+    let newValues = await invoke("update_processor_values", {
+      processorName: "amplifier",
+      values: { volume: 1.0 },
+    });
+    console.log(newValues);
   }
 </script>
 
 <div>
-  <select on:change={(e) => setInputDevice(e)}>
-    {#each devices.inputs as input}
-      <option value="{input}" selected={input == initialDevices.input}>{input}</option>
-    {/each}
-  </select>
-  <select on:change={(e) => setOutDevice(e)}>
-    {#each devices.outputs as output}
-      <option value="{output}" selected={output == initialDevices.output}>{output}</option>
-    {/each}
-  </select>
-  <div>
-
-    {#each processors as processor }
-      <button on:click={(e) => addProcessor(e)}>{processor}</button>
-    {/each}
-
-  </div>
+  {#each availableProcessors as processor}
+    <button on:click={(e) => addProcessor(e)}>{processor}</button>
+  {/each}
 
   <div>
     <button on:click={updateProcessorValues}>Turn volume to 1</button>
