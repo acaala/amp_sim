bcd7369588ff92a4cc665fe12bfec8638cfad00f
Liam Watts <elnewatts@gmail.com>
2024-01-09 19:14:20 +0000

    updated button class usage

--- src/lib/Processors.svelte
+++ src/lib/Processors.svelte
@@ -73,12 +73,12 @@
 
 <div class="flex w-full h-full">
   <div class="w-1/3 py-4 px-6">
-    <h2 class="text-xl text-gray-300 font-medium mb-4 ">Audio Effects</h2>
+    <h2 class="text-xl text-gray-300 font-medium mb-4">Audio Effects</h2>
 
     <div class="flex flex-col gap-4 mb-4">
       {#each availableProcessors as processor}
         <button
-          class="border border-[#2f2f2f] capitalize"
+          class="button border border-[#2f2f2f] capitalize"
           on:click={(e) => addProcessor(e)}>{processor}</button
         >
       {/each}
@@ -90,10 +90,15 @@
       Active Processors
     </h4>
     {#each activeProcessors as processor}
-      <form name={processor.name.Str} class="bg-[#333] border border-gray-800 p-2">
+      <form
+        name={processor.name.Str}
+        class="bg-[#333] border border-gray-800 p-2"
+      >
         <div class="flex justify-between mb-4 items-center">
           <div><p class="text-xl capitalize">{processor.name.Str}</p></div>
-          <button on:click={(e) => removeProcessor(e)}>Remove</button>
+          <button class="button" on:click={(e) => removeProcessor(e)}
+            >Remove</button
+          >
         </div>
         <div class="flex justify-between">
           {#each Object.entries(processor.details.Map) as [detail, value]}
