cb332bbb0b44220fe0c1937d0bc4adad4b49d8a8
Liam Watts <elnewatts@gmail.com>
2024-01-12 14:22:46 +0000

    - commented assistant component

---
+++ src/lib/Assistant.svelte
@@ -0,0 +1,42 @@
+<!-- <script>
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { onMount } from "svelte";
+
+  export let hideAssistant;
+  let apiKey = "";
+
+  async function submitPromptToAi() {
+    // await invoke("submit_user_prompt_to_ai", { prompt: "what is the date?" });
+    await invoke("submit_user_prompt", { prompt: "give me a crunchy tone" });
+  }
+
+  async function setApiKey() {
+    // Need to confirm valid key.
+    await invoke("set_openai_api_key", { key: apiKey.trim() });
+  }
+
+  onMount(async () => {
+    apiKey = await invoke("get_openai_api_key");
+    if (apiKey) {
+      await invoke("init_assistant");
+    }
+  });
+</script>
+
+<div class="absolute inset-0 flex items-center justify-center backdrop-blur-sm">
+  <div class="bg-[#333] p-4 rounded-md">
+    {#if !apiKey}
+      <div class="flex items-center gap-4 w-full justify-between">
+        <p class="text-sm font-medium text-gray-300">API Key:</p>
+        <input type="password" bind:value={apiKey} on:change={setApiKey} />
+      </div>
+    {:else}
+      <div class="flex items-center">
+        <h2>Ai Assistant</h2>
+        <button on:click={() => (hideAssistant = true)}>Close</button>
+      </div>
+      <input type="text" />
+      <button on:click={() => submitPromptToAi()}>Sub</button>
+    {/if}
+  </div>
+</div> -->
